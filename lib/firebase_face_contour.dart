2db7274d10ba5a53b5754621bd0d8d55cd91dfec
Gian Marco Di Francesco <difrancescogianmarco@gmail.com>
2019-04-04 23:28:43 +0200

    add miss files

---
+++ lib/firebase_face_contour.dart
@@ -0,0 +1,12 @@
+library firebase_face_contour;
+
+import 'dart:async';
+import 'dart:io';
+import 'dart:typed_data';
+import 'dart:ui';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
+
+part 'src/face_detector.dart';
+part 'src/firebase_vision.dart';
